3d10356eae8a9efbc79b34e767240bf680d38811
Dmitry Aleshkin <99013503+Harned@users.noreply.github.com>
2024-02-26 18:37:42 +0600

    FixAssets Commit

---
+++ iosAleshkin/Models/Model/Doctor.swift
@@ -0,0 +1,16 @@
+//
+//  Doctor.swift
+//  iosAleshkin
+//
+//  Created by Nes on 26.02.2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct Doctor {
+    var firstName: String
+    var lastName: String
+    var specialization: String
+    var image: Image?
+}
